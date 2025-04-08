24e8d5a9dcc2397ae4409857b1c42ba6161acabe
Mikhail.Rodionychev <tralllgm@gmail.com>
2025-04-09 01:35:34 +0200

    configure frontend to run on 5000

--- run_local.sh
+++ run_local.sh
@@ -13,7 +13,7 @@ cd FeaturesProvider && API_KEY=$API_KEY uvicorn main:app --reload 2> ../logs/fea
 echo "Running backend at localhost:8081 and feature provider at 127.0.0.1:8000..."
 cd BackEnd && PORT=8081 PROVIDER_HOST=127.0.0.1 PROVIDER_PORT=8000 ./gradlew run 2> ../logs/backend.err > ../logs/backend.out &
 
-echo "Running web server with backend at localhost:8081..."
-cd WebInterface && VITE_API_HOST=localhost VITE_API_PORT=8081 npm run dev 2> ../logs/web.err > ../logs/web.out &
+echo "Running web server at localhost:5000 with backend at localhost:8081..."
+cd WebInterface && VITE_DEV_HOST=localhost VITE_DEV_PORT=5000 VITE_API_HOST=localhost VITE_API_PORT=8081 npm run dev 2> ../logs/web.err > ../logs/web.out &
 
 wait
