6ef4e8f524713f39c29c4aaf760006715979f0ba
FerrumBrain <lemur1416@gmail.com>
2025-04-09 01:19:03 +0200

    added features handle button

--- WebInterface/src/pages/Home.tsx
+++ WebInterface/src/pages/Home.tsx
@@ -1,10 +1,12 @@
-import { FetchButton } from "../components/FetchButton";
+import { FetchHelloButton } from "../components/FetchHelloButton.tsx";
+import {FetchFeaturesHelloButton} from "../components/FetchFeaturesHelloButton.tsx";
 
 export default function Home() {
     return (
         <main style={{ padding: "2rem" }}>
             <h1>Frontend Boilerplate</h1>
-            <FetchButton />
+            <FetchHelloButton />
+            <FetchFeaturesHelloButton />
         </main>
     );
 }
