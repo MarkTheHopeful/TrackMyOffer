bc7c4c4f191dd442f45122cb7f7d581b96e12289
Mikhail.Rodionychev <tralllgm@gmail.com>
2025-04-08 22:39:19 +0200

    add trivial server setup

---
+++ BackEnd/src/main/kotlin/cub/trackmyoffer/Application.kt
@@ -0,0 +1,26 @@
+package cub.trackmyoffer
+
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.server.application.*
+import io.ktor.server.netty.*
+import io.ktor.server.routing.*
+
+fun main(args: Array<String>) = EngineMain.main(args)
+
+fun Application.module() {
+    val fProviderHost = environment.config.propertyOrNull("ktor.feature_provider.host")?.getString() ?: "0.0.0.0"
+    val fProviderPort = environment.config.propertyOrNull("ktor.feature_provider.port")?.getString() ?: "8081"
+
+    val httpClient = HttpClient(CIO) {
+        expectSuccess = false
+        engine {
+            requestTimeout = 10_000
+        }
+    }
+
+    routing {
+        backendRouting()
+        featureProviderRouting(httpClient, FeatureProviderRoutingConfig(fProviderHost, fProviderPort))
+    }
+}
