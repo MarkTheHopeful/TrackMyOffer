fb6c02fb11f39e881193801a92fe27edb1629278
FerrumBrain <lemur1416@gmail.com>
2025-05-11 23:33:02 +0200

    endpoint logic

--- BackEnd/src/main/kotlin/cub/trackmyoffer/FeatureProviderRouting.kt
+++ BackEnd/src/main/kotlin/cub/trackmyoffer/FeatureProviderRouting.kt
@@ -7,6 +7,8 @@ import io.ktor.http.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
+data class Response(val status: HttpStatusCode, val body: String)
+
 data class FeatureProviderRoutingConfig(val remoteHost: String, val remotePort: String) {
     val remote = "http://$remoteHost:$remotePort"
 }
@@ -31,6 +33,73 @@ fun Route.featureProviderRouting(httpClient: HttpClient, config: FeatureProvider
 
             get("/") {
                 val response = httpClient.get("${config.remote}/")
+                call.respondText(response.bodyAsText(), status = response.status)
+            }
+
+            suspend fun extractJobDescription(call: RoutingCall): Response {
+                val jobDescription = call.request.queryParameters["jobDescriptionLink"]
+                    ?: return Response(HttpStatusCode.BadRequest, "Missing jobDescriptionLink parameter")
+
+                val response = httpClient.post("${config.remote}/extract-job-description") {
+                    contentType(ContentType.Application.Json)
+                    setBody("""{"jobDescription": "$jobDescription"}""")
+                }
+                return Response(response.status, response.bodyAsText())
+            }
+
+            get("/match-position") {
+                val extractorResponse = extractJobDescription(call)
+                if (extractorResponse.status != HttpStatusCode.OK) {
+                    call.respond(extractorResponse.status, extractorResponse.body)
+                    return@get
+                }
+
+                val response = httpClient.post("${config.remote}/match-position") {
+                    contentType(ContentType.Application.Json)
+                    setBody(extractorResponse.body)
+                }
+                call.respondText(response.bodyAsText(), status = response.status)
+            }
+
+
+            get("/build-cv") {
+                val extractorResponse = extractJobDescription(call)
+                if (extractorResponse.status != HttpStatusCode.OK) {
+                    call.respond(extractorResponse.status, extractorResponse.body)
+                    return@get
+                }
+
+                val response = httpClient.post("${config.remote}/build-cv") {
+                    contentType(ContentType.Application.Json)
+                    setBody(extractorResponse.body)
+                }
+                call.respondText(response.bodyAsText(), status = response.status)
+            }
+
+            get("/generate-motivational-letter") {
+                val extractorResponse = extractJobDescription(call)
+                if (extractorResponse.status != HttpStatusCode.OK) {
+                    call.respond(extractorResponse.status, extractorResponse.body)
+                    return@get
+                }
+
+                val textStyle = call.request.queryParameters["textStyle"]
+                val notes = call.request.queryParameters["notes"]
+
+                val response = httpClient.post("${config.remote}/generate-motivational-letter") {
+                    contentType(ContentType.Application.Json)
+                    setBody(
+                        "${
+                            extractorResponse.body.trimEnd().removeSuffix("}")
+                        }${
+                            if (textStyle != null) """, "textStyle": "$textStyle""""
+                            else ""
+                        }${
+                            if (notes != null) """, "notes": "$notes""""
+                            else ""
+                        }}"
+                    )
+                }
                 call.respondText(response.bodyAsText(), status = response.status)
             }
         }
